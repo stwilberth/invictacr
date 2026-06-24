281119656154de596d13fa2504e39c5b5b250389
Wilberth Loría <stwilberth@gmail.com>
2026-06-23 22:14:49 -0600

    Add Alpine.js CDN to enable navbar dropdown and search
    The navbar Soporte dropdown and the search bar relied on Alpine.js
    directives (x-data, x-show, @click, x-model) but Alpine was never
    loaded. Add the CDN script to app-layout with defer so it initializes
    after inline component scripts define their state functions.

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -28,6 +28,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
     </noscript>
 
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
     <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossorigin />
     <link rel="preconnect" href="https://www.googletagmanager.com" crossorigin />
     <link rel="preconnect" href="https://connect.facebook.net" crossorigin />
